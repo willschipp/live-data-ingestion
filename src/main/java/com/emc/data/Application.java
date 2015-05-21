4d7be52218a3057655c65ff934993ad304b53de3
willschipp <will.schipp@gmail.com>
2015-05-21 06:24:41 +0400

    module changes

--- src/main/java/com/emc/data/Application.java
+++ src/main/java/com/emc/data/Application.java
@@ -1,6 +1,7 @@
 package com.emc.data;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class Application {
